b8dcdcfb9d170e756a4f9a740c57f51987f751de
Lucian Cristian <lucian.cristian@gmail.com>
2020-04-21 00:13:00 +0300

    rk3328: add support for Radxa Rock Pi E
    also update ATF rockchip to final release
    
    Signed-off-by: Lucian Cristian <lucian.cristian@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -4,11 +4,20 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+define Device/radxa_rockpi-e
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := RockPi E
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := rockpi-e-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rockchip64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += radxa_rockpi-e
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rockchip64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
